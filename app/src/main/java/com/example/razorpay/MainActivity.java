8c72fd487ead9ccdc6783c75ff08f8126f5321c5
Prateek-Nayak <nprateek24@gmail.com>
2022-07-03 16:17:03 +0530

    Draft ready

--- app/src/main/java/com/example/razorpay/MainActivity.java
+++ app/src/main/java/com/example/razorpay/MainActivity.java
@@ -6,6 +6,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.razorpay.Checkout;
@@ -16,9 +17,12 @@ import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity implements PaymentResultListener {
 
-    // variables for our
-    // edit text and button.
+    // variables for edit ext and button.
     private EditText amountEdt;
+    private EditText idname;
+    private EditText idusn;
+    private EditText idemail;
+    private EditText idphone;
     private Button payBtn;
 
     @Override
@@ -27,8 +31,12 @@ public class MainActivity extends AppCompatActivity implements PaymentResultList
         setContentView(R.layout.activity_main);
 
         // initializing all our variables.
-        amountEdt = findViewById(R.id.idEdtAmount);
+        amountEdt = findViewById(R.id.idAmount);
         payBtn = findViewById(R.id.idBtnPay);
+        idname = findViewById(R.id.name);
+        idusn = findViewById(R.id.usn);
+        idemail = findViewById(R.id.email);
+        idphone = findViewById(R.id.phone);
 
         // adding on click listener to our button.
         payBtn.setOnClickListener(new View.OnClickListener() {
@@ -37,7 +45,10 @@ public class MainActivity extends AppCompatActivity implements PaymentResultList
                 // on below line we are getting
                 // amount that is entered by user.
                 String sAmount = amountEdt.getText().toString();
-
+                String usn = idusn.getText().toString();
+                String name = idname.getText().toString();
+                String phone = idphone.getText().toString();
+                String email = idemail.getText().toString();
                 // rounding off the amount.
                 int amount = Math.round(Float.parseFloat(sAmount) * 100);
 
@@ -48,19 +59,19 @@ public class MainActivity extends AppCompatActivity implements PaymentResultList
                 checkout.setKeyID("rzp_test_XU3Qiw0dILWizs");
 
                 // set image
-                checkout.setImage(R.drawable.prateek);
+//                checkout.setImage(R.drawable.logo);
 
                 // initialize json object
                 JSONObject object = new JSONObject();
                 try {
                     // to put name
-                    object.put("name", "Prateek Nayak");
+                    object.put("name", "Bangalore Institute of Technology");
 
                     // put description
-                    object.put("description", "Test payment");
+                    object.put("description", name + " - " + usn);
 
                     // to set theme color
-                    object.put("theme.color", "");
+                    object.put("theme.color", "#50C878");
 
                     // put the currency
                     object.put("currency", "INR");
@@ -69,10 +80,10 @@ public class MainActivity extends AppCompatActivity implements PaymentResultList
                     object.put("amount", amount);
 
                     // put mobile number
-                    object.put("prefill.contact", "9284064503");
+                    object.put("prefill.contact", phone);
 
                     // put email
-                    object.put("prefill.email", "chaitanyamunje@gmail.com");
+                    object.put("prefill.email", email);
 
                     // open razorpay to checkout activity
                     checkout.open(MainActivity.this, object);
@@ -86,6 +97,11 @@ public class MainActivity extends AppCompatActivity implements PaymentResultList
     @Override
     public void onPaymentSuccess(String s) {
         // this method is called on payment success.
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Payment ID");
+        builder.setMessage("Please note this ID for future reference\n"+s);
+//        builder.setMessage(s);
+        builder.show();
         Toast.makeText(this, "Payment is successful : " + s, Toast.LENGTH_SHORT).show();
     }
 
